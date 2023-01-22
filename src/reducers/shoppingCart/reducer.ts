867e6df5a808c7e3adb7eed11128776d786e9bce
Larissa Pissurno <larissapissurno@gmail.com>
2023-01-22 18:34:54 -0300

    refact: update shoppingCart reducer

--- src/reducers/shoppingCart/reducer.ts
+++ src/reducers/shoppingCart/reducer.ts
@@ -1,4 +1,5 @@
 import produce from 'immer'
+import { WritableDraft } from 'immer/dist/internal'
 import { IAction, EActionTypes, TUpdateCoffeeAction } from './actions'
 
 export interface Coffee {
@@ -10,8 +11,12 @@ export interface Coffee {
   imageUrl: string
 }
 
-export interface ShoppingCartItem extends Coffee {
+export interface ShoppingCartItem {
+  id: string
+  name: string
+  price: number
   quantity: number
+  imageUrl: string
 }
 
 interface ShoppingCartState {
@@ -26,34 +31,49 @@ export function shoppingCartReducer(
     EActionTypes,
     (state: ShoppingCartState, action: any) => ShoppingCartState
   >([
-    [EActionTypes.UPDATE_COFFEE_QUANTITY, updateCoffee],
+    [EActionTypes.UPDATE_COFFEE_QUANTITY, updateCoffeeQuantity],
     [EActionTypes.REMOVE_COFFEE, removeCoffee],
   ])
 
   return map.has(action.type) ? map.get(action.type)!(state, action) : state
 }
 
-function updateCoffee(
+function updateCoffeeQuantity(
   state: ShoppingCartState,
   action: IAction<TUpdateCoffeeAction>,
 ): ShoppingCartState {
+  const addCoffeeToCart = (
+    draft: WritableDraft<ShoppingCartState>,
+    coffee: Coffee,
+    quantity: number,
+  ) => {
+    draft.items.push({
+      ...coffee,
+      quantity,
+    })
+  }
+
+  const removeCoffeFromCart = (
+    draft: WritableDraft<ShoppingCartState>,
+    existentCoffee: WritableDraft<ShoppingCartItem>,
+  ) => {
+    draft.items = draft.items.filter(
+      (coffee) => coffee.id !== existentCoffee.id,
+    )
+  }
+
   return produce(state, (draft) => {
     const { coffee, quantity } = action.payload
     const existentCoffee = draft.items.find((item) => item.id === coffee.id)
 
     if (existentCoffee && quantity === 0) {
-      draft.items = draft.items.filter(
-        (coffee) => coffee.id !== existentCoffee.id,
-      )
+      removeCoffeFromCart(draft, existentCoffee)
 
       return
     }
 
     if (!existentCoffee && quantity > 0) {
-      draft.items.push({
-        ...coffee,
-        quantity,
-      })
+      addCoffeeToCart(draft, coffee, quantity)
 
       return
     }
